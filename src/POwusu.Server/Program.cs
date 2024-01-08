e7218f8f1eb2fac0c20d064d96a3ca5b67477f68
Prince Owusu <prince.owusu@hubtel.com>
2024-01-08 01:06:59 +0000

    feat: Add serilog implementation

--- src/POwusu.Server/Program.cs
+++ src/POwusu.Server/Program.cs
@@ -15,132 +15,155 @@ using System.Text.Json;
 using IValidator = POwusu.Server.Extensions.Validation.IValidator;
 using Humanizer;
 using POwusu.Server.Middlewares;
+using Serilog;
+using Serilog.Settings.Configuration;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Configure options to the container.
-
-ValidatorOptions.Global.DefaultClassLevelCascadeMode = CascadeMode.Continue;
-ValidatorOptions.Global.DefaultRuleLevelCascadeMode = CascadeMode.Stop;
-ValidatorOptions.Global.DisplayNameResolver = (type, memberInfo, expression) =>
+try
 {
-    string? RelovePropertyName()
+    Log.Information("Starting web application...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration,
+        new ConfigurationReaderOptions { SectionName = "SerilogOptions" }).Enrich.FromLogContext().CreateLogger();
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseSerilog(Log.Logger);
+
+    // Configure options to the container.
+
+    ValidatorOptions.Global.DefaultClassLevelCascadeMode = CascadeMode.Continue;
+    ValidatorOptions.Global.DefaultRuleLevelCascadeMode = CascadeMode.Stop;
+    ValidatorOptions.Global.DisplayNameResolver = (type, memberInfo, expression) =>
     {
-        if (expression != null)
+        string? RelovePropertyName()
         {
-            var chain = FluentValidation.Internal.PropertyChain.FromExpression(expression);
-            if (chain.Count > 0) return chain.ToString();
+            if (expression != null)
+            {
+                var chain = FluentValidation.Internal.PropertyChain.FromExpression(expression);
+                if (chain.Count > 0) return chain.ToString();
+            }
+
+            if (memberInfo != null)
+            {
+                return memberInfo.Name;
+            }
+
+            return null;
         }
 
-        if (memberInfo != null)
-        {
-            return memberInfo.Name;
-        }
+        return RelovePropertyName()?.Humanize();
+    };
 
-        return null;
+    builder.Services.ConfigureHttpJsonOptions(options =>
+    {
+        options.SerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+        options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+        options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+
+        options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
+    // Add services to the container.
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlite(builder.Configuration.GetConnectionString("AppConnection"));
+    });
+
+    builder.Services.AddIdentity<User, Role>(options =>
+    {
+        // Password settings. (Will be using fluent validation)
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequiredUniqueChars = 0;
+
+        // Lockout settings.
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+
+        // User settings.
+        options.User.AllowedUserNameCharacters = string.Empty;
+        options.User.RequireUniqueEmail = false;
+
+        options.SignIn.RequireConfirmedAccount = false;
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+
+        // Generate Short Code for Email Confirmation using Asp.Net Identity core 2.1
+        // source: https://stackoverflow.com/questions/53616142/generate-short-code-for-email-confirmation-using-asp-net-identity-core-2-1
+        options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider;
+        options.Tokens.ChangeEmailTokenProvider = TokenOptions.DefaultEmailProvider;
+        options.Tokens.PasswordResetTokenProvider = TokenOptions.DefaultEmailProvider;
+
+        options.ClaimsIdentity.RoleClaimType = ClaimTypes.Role;
+        options.ClaimsIdentity.UserNameClaimType = ClaimTypes.Name;
+        options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
+        options.ClaimsIdentity.EmailClaimType = ClaimTypes.Email;
+        options.ClaimsIdentity.SecurityStampClaimType = ClaimTypes.SerialNumber;
+    })
+        .AddEntityFrameworkStores<AppDbContext>()
+        .AddDefaultTokenProviders()
+        .AddClaimsPrincipalFactory<UserClaimsPrincipalFactory>();
+
+    builder.Services.AddTransient<IJwtTokenManager, JwtTokenManager>();
+
+    builder.Services.ConfigureOptions<ConfigureJwtBearerOptions>();
+
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    })
+        .AddJwtBearer().AddJwtTokenManager();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddScoped<IValidator, Validator>();
+    builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+    builder.Services.AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+    builder.Services.AddScoped<IIdentityService, IdentityService>();
+
+    builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
+    builder.Services.AddHostedService<ProgramService>();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
 
-    return RelovePropertyName()?.Humanize();
-};
+    app.UseHttpsRedirection();
 
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    app.UseDbTransaction<AppDbContext>();
 
-    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+    app.MapEndpoints();
 
-    options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-});
-
-// Add services to the container.
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-{
-    options.UseSqlite(builder.Configuration.GetConnectionString("AppConnection"));
-});
-
-builder.Services.AddIdentity<User, Role>(options =>
-{
-    // Password settings. (Will be using fluent validation)
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 0;
-    options.Password.RequiredUniqueChars = 0;
-
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
-
-    // User settings.
-    options.User.AllowedUserNameCharacters = string.Empty;
-    options.User.RequireUniqueEmail = false;
-
-    options.SignIn.RequireConfirmedAccount = false;
-    options.SignIn.RequireConfirmedEmail = false;
-    options.SignIn.RequireConfirmedPhoneNumber = false;
-
-    // Generate Short Code for Email Confirmation using Asp.Net Identity core 2.1
-    // source: https://stackoverflow.com/questions/53616142/generate-short-code-for-email-confirmation-using-asp-net-identity-core-2-1
-    options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider;
-    options.Tokens.ChangeEmailTokenProvider = TokenOptions.DefaultEmailProvider;
-    options.Tokens.PasswordResetTokenProvider = TokenOptions.DefaultEmailProvider;
-
-    options.ClaimsIdentity.RoleClaimType = ClaimTypes.Role;
-    options.ClaimsIdentity.UserNameClaimType = ClaimTypes.Name;
-    options.ClaimsIdentity.UserIdClaimType = ClaimTypes.NameIdentifier;
-    options.ClaimsIdentity.EmailClaimType = ClaimTypes.Email;
-    options.ClaimsIdentity.SecurityStampClaimType = ClaimTypes.SerialNumber;
-})
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders()
-    .AddClaimsPrincipalFactory<UserClaimsPrincipalFactory>();
-
-builder.Services.AddTransient<IJwtTokenManager, JwtTokenManager>();
-
-builder.Services.ConfigureOptions<ConfigureJwtBearerOptions>();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-
-})
-    .AddJwtBearer().AddJwtTokenManager();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddScoped<IValidator, Validator>();
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-builder.Services.AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-
-builder.Services.AddScoped<IIdentityService, IdentityService>();
-
-builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
-
-builder.Services.AddHostedService<ProgramService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.Run();
 }
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly.");
 
-app.UseHttpsRedirection();
-
-app.UseDbTransaction<AppDbContext>();
-
-app.MapEndpoints();
-
-app.Run();
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
