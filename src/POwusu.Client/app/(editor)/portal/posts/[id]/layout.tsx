2a704e75d9f60497ec7ae9a90f4b7da267820c4b
Prince Owusu <prince.owusu@hubtel.com>
2024-03-16 21:06:59 +0000

    Commit

--- src/POwusu.Client/app/(editor)/portal/posts/[id]/layout.tsx
+++ src/POwusu.Client/app/(editor)/portal/posts/[id]/layout.tsx
@@ -3,5 +3,5 @@ interface EditorLayoutProps {
 }
 
 export default function EditorLayout({ children }: EditorLayoutProps) {
-  return <div className="container max-w-[1400px] mx-auto grid h-full items-start gap-10 pt-8">{children}</div>;
+  return <div className="container mx-auto grid h-full max-w-[1400px] items-start gap-10 pt-8">{children}</div>;
 }
--- src/POwusu.Server/Entities/Blog/Post.cs
+++ src/POwusu.Server/Entities/Blog/Post.cs
@@ -1,4 +1,5 @@
 ﻿using POwusu.Server.Entities.Identity;
+using System.ComponentModel;
 
 namespace POwusu.Server.Entities.Blog
 {
@@ -11,6 +12,8 @@ namespace POwusu.Server.Entities.Blog
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
         public string Title { get; set; } = null!;
+
+        public PostCategory Category { get; set; }
 
         public string? Summary { get; set; } = null!;
 
@@ -35,5 +38,35 @@ namespace POwusu.Server.Entities.Blog
         public bool Deleted { get; set; }
 
         public long ReadingDuration { get; set; }
+    }
+
+    public enum PostCategory
+    {
+        Others,
+        [Description("Automotive")]
+        Automotive,
+        BusinessAndIndustrial,
+        CareerAndJobs,
+        Economy,
+        Education,
+        Entertainment,
+        [Description("Fashion & Beauty")]
+        FashionAndBeauty,
+        [Description("Food & Drinks")]
+        FoodAndDrinks,
+        HealthAndFitness,
+        Lifestyle,
+        [Description("Politics & Policy")]
+        PoliticsAndPolicy,
+        [Description("Public Safety")]
+        PublicSafety,
+        [Description("Pets & Animals")]
+        PetsAndAnimals,
+        ReligionAndBelief,
+        [Description("Relationships & Parenting")]
+        RelationshipsAndParenting,
+        SocietyNews,
+        Sports,
+        Technology
     }
 }
